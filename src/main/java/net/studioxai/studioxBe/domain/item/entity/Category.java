b2fd751ae2a1eb2374774e859bf191e2f011c1e0
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-09-19 04:51:56 +0900

    :sparkles: Item 엔티티 추가

---
+++ src/main/java/net/studioxai/studioxBe/domain/item/entity/Category.java
@@ -0,0 +1,5 @@
+package net.studioxai.studioxBe.domain.item.entity;
+
+public enum Category {
+    MODEL, STUDIO, IMAGE
+}
---
+++ src/main/java/net/studioxai/studioxBe/domain/item/entity/Item.java
@@ -0,0 +1,29 @@
+package net.studioxai.studioxBe.domain.item.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import net.studioxai.studioxBe.global.entity.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "items")
+public class Item extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_idx")
+    private Long itemIdx;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category")
+    private Category category;
+
+    @Column(name = "url")
+    private String url;
+
+}
