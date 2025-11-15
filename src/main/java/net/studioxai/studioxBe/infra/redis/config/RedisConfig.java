1ad569c03618375c5df33abbddc163ed5c4b6b7a
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-11-15 23:21:21 +0900

    :bug: @Value의 password 수정

--- src/main/java/net/studioxai/studioxBe/infra/redis/config/RedisConfig.java
+++ src/main/java/net/studioxai/studioxBe/infra/redis/config/RedisConfig.java
@@ -22,7 +22,7 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-    @Value("spring.data.redis.password")
+    @Value("${spring.data.redis.password}")
     private String password;
 
     @Bean
