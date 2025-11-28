784dfb35b42a558b69d7a108e9d39f26758c6d67
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-11-28 16:38:14 +0900

    feat: manager 관련 엔티티 구현

---
+++ src/main/java/net/studioxai/studioxBe/domain/project/ProjectManager.java
@@ -0,0 +1,27 @@
+package net.studioxai.studioxBe.domain.project;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import net.studioxai.studioxBe.domain.project.entity.Project;
+import net.studioxai.studioxBe.domain.user.entity.User;
+import net.studioxai.studioxBe.global.entity.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "project_managers")
+public class ProjectManager extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_manager_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
