0c5280486651a4f5b6e6ea6e688ba9265034af38
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-12-07 15:52:01 +0900

    refactor: 폴더매니저 추가 로직 수정

--- src/main/java/net/studioxai/studioxBe/domain/folder/service/FolderManagerService.java
+++ src/main/java/net/studioxai/studioxBe/domain/folder/service/FolderManagerService.java
@@ -2,12 +2,16 @@ package net.studioxai.studioxBe.domain.folder.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.studioxai.studioxBe.domain.folder.dto.FolderCreateRequest;
+import net.studioxai.studioxBe.domain.folder.dto.request.FolderCreateRequest;
+import net.studioxai.studioxBe.domain.folder.dto.request.FolderManagerAddRequest;
 import net.studioxai.studioxBe.domain.folder.entity.Folder;
 import net.studioxai.studioxBe.domain.folder.entity.FolderManager;
+import net.studioxai.studioxBe.domain.folder.exception.FolderManagerErrorCode;
+import net.studioxai.studioxBe.domain.folder.exception.FolderManagerExceptionHandler;
 import net.studioxai.studioxBe.domain.folder.repository.FolderManagerBulkRepository;
 import net.studioxai.studioxBe.domain.folder.repository.FolderManagerRepository;
 import net.studioxai.studioxBe.domain.user.entity.User;
+import net.studioxai.studioxBe.domain.user.service.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,8 +24,31 @@ import java.util.List;
 public class FolderManagerService {
     private final FolderManagerRepository folderManagerRepository;
     private final FolderManagerBulkRepository folderManagerBulkRepository;
+    private final UserService userService;
+
+    public void existFolderMangersOrThrow(Long userId, List<FolderManager> managers) {
+        boolean exists = managers.stream().anyMatch(manager -> manager.getUser().getId().equals(userId));
+        if (!exists) {
+            throw new FolderManagerExceptionHandler(FolderManagerErrorCode.USER_NO_FOLDER_AUTHORITY);
+        }
+    }
+
+    public List<FolderManager> getFolderMangersOrThrow(Long folderId) {
+        List<FolderManager> managers = folderManagerRepository.findByFolderId(folderId);
+        if (managers.isEmpty()) {
+            throw new FolderManagerExceptionHandler(FolderManagerErrorCode.FOLDER_NOT_FOUND);
+        }
+        return managers;
+    }
+
     @Transactional
-    public void addManager(User user, Folder folder) {
+    public void addManager(Long userId, Long folderId, FolderManagerAddRequest folderManagerAddRequest) {
+        List<FolderManager> managers = getFolderMangersOrThrow(folderId);
+        existFolderMangersOrThrow(userId, managers);
+        Folder folder = managers.get(0).getFolder();
+
+        User user = userService.getUserByEmailOrThrow(folderManagerAddRequest.email());
+
         FolderManager folderManager = FolderManager.create(user, folder);
         folderManagerRepository.save(folderManager);
     }
