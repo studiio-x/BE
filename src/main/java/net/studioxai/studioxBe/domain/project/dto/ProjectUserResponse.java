d202775e3c5695484dae282096bca0648089f472
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-12-08 17:43:56 +0900

    refactor: username으로 변수명 수정

--- src/main/java/net/studioxai/studioxBe/domain/project/dto/ProjectUserResponse.java
+++ src/main/java/net/studioxai/studioxBe/domain/project/dto/ProjectUserResponse.java
@@ -2,7 +2,7 @@ package net.studioxai.studioxBe.domain.project.dto;
 
 public record ProjectUserResponse (
         Long userId,
-        String userName,
+        String username,
         String email,
         String profileImageUrl
 ) {
