8a1f412d6091fb0d3b1ace5d36e061ef9e9f877e
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2026-01-05 08:01:10 +0900

    feat: swagger 로그인 로직 추가

---
+++ src/main/java/net/studioxai/studioxBe/global/util/EnvironmentUtil.java
@@ -0,0 +1,46 @@
+package net.studioxai.studioxBe.global.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class EnvironmentUtil {
+    private final Environment environment;
+
+    private final String BLUE = "blue";
+    private final String GREEN = "green";
+    private final String LOCAL = "local";
+
+    private final List<String> PROD = List.of("blue", "green");
+
+    public Boolean isBlueProfile() {
+        String[] activeProfiles = environment.getActiveProfiles();
+        List<String> currentProfile = Arrays.stream(activeProfiles).toList();
+        return currentProfile.contains(BLUE);
+    }
+
+    public Boolean isGreebProfile() {
+        String[] activeProfiles = environment.getActiveProfiles();
+        List<String> currentProfile = Arrays.stream(activeProfiles).toList();
+        return currentProfile.contains(GREEN);
+    }
+
+    public Boolean isLocalProfile() {
+        String[] activeProfiles = environment.getActiveProfiles();
+        List<String> currentProfile = Arrays.stream(activeProfiles).toList();
+        return currentProfile.contains(LOCAL);
+    }
+
+    public Boolean isProdAndStagingProfile() {
+        String[] activeProfiles = environment.getActiveProfiles();
+        List<String> currentProfile = Arrays.stream(activeProfiles).toList();
+        return CollectionUtils.containsAny(PROD, currentProfile);
+    }
+
+}
