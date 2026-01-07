4d0cfde0121fbc434d4e4fb6d1e8928dbf585826
Seoah <0325sandy@ewhain.net>
2026-01-07 15:10:13 +0900

    refactor: exception 추가

--- src/main/java/net/studioxai/studioxBe/domain/template/service/TemplateService.java
+++ src/main/java/net/studioxai/studioxBe/domain/template/service/TemplateService.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import net.studioxai.studioxBe.domain.template.dto.response.TemplateByCategoryResponse;
 import net.studioxai.studioxBe.domain.template.dto.response.TemplateByKeywordResponse;
 import net.studioxai.studioxBe.domain.template.entity.TemplateKeywordType;
+import net.studioxai.studioxBe.domain.template.exception.TemplateManagerErrorCode;
+import net.studioxai.studioxBe.domain.template.exception.TemplateManagerExceptionHandler;
 import net.studioxai.studioxBe.domain.template.repository.TemplateKeywordRepository;
 import net.studioxai.studioxBe.domain.template.repository.TemplateRepository;
 import net.studioxai.studioxBe.global.entity.enums.Category;
@@ -21,13 +23,25 @@ public class TemplateService {
     private final TemplateKeywordRepository templateKeywordRepository;
 
     public List<TemplateByCategoryResponse> getTemplatesByCategory(Category category) {
-        return templateRepository.findByCategoryOrderByCreatedAtDesc(category);
+        List<TemplateByCategoryResponse> results = templateRepository.findByCategoryOrderByCreatedAtDesc(category);
+
+        if (results.isEmpty()) {
+            throw new TemplateManagerExceptionHandler(TemplateManagerErrorCode.TEMPLATE_NOT_FOUND_BY_CATEGORY);
+        }
+
+        return results;
     }
 
 
     public List<TemplateByKeywordResponse> getTemplatesByKeyword(TemplateKeywordType keyword) {
-        return templateKeywordRepository
-                .findByKeywordOrderByTemplateCreatedAtDesc(keyword);
+        List<TemplateByKeywordResponse> results = templateKeywordRepository.findByKeywordOrderByTemplateCreatedAtDesc(keyword);
+
+        if (results.isEmpty()) {
+            throw new TemplateManagerExceptionHandler(TemplateManagerErrorCode.TEMPLATE_NOT_FOUND_BY_KEYWORD);
+        }
+
+        return results;
+
     }
 
 }
