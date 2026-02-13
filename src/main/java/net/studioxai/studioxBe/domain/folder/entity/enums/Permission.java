ba24547fbe2826b92effaef81ffa8c36aa24fe08
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2026-02-13 14:07:43 +0900

    fix: 변수명 및 매개변수 순서 변경

--- src/main/java/net/studioxai/studioxBe/domain/folder/entity/enums/Permission.java
+++ src/main/java/net/studioxai/studioxBe/domain/folder/entity/enums/Permission.java
@@ -6,14 +6,15 @@ import net.studioxai.studioxBe.domain.folder.exception.FolderManagerExceptionHan
 public enum Permission {
     OWNER(true, true, true),
     FULL_ACCESS(true, true, true),
+    WRITE(true, true, false),
     READ(true, false, false),
-    WRITE(true, true, false),;
+    ;
 
     private final boolean canShare;
     private final boolean canWrite;
     private final boolean canRead;
 
-    Permission(boolean canWrite, boolean canShare,boolean canRead) {
+    Permission(boolean canRead, boolean canWrite, boolean canShare) {
         this.canWrite = canWrite;
         this.canRead = canRead;
         this.canShare = canShare;
